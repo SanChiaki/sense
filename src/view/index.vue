a1de1a949ef5dbb7c54a9582a0739f6508eb269c
Blanceluo <hasayaki97@gmail.com>
2018-11-20 10:21:29 +0800

    add modal

--- src/view/index.vue
+++ src/view/index.vue
@@ -21,7 +21,7 @@ export default {
   },
   data() {
     return {
-      routes: ['button', 'checkbox', 'input', 'select', 'tag', 'badge', 'alert', 'radio', 'pagination']
+      routes: ['button', 'checkbox', 'input', 'select', 'tag', 'badge', 'alert', 'radio', 'pagination', 'modal']
     }
   },
   methods: {
